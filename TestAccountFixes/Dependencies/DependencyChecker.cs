7e7ad3a1082a96fafcbb6acd102e175b03a0d986
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2024-06-21 15:29:17 +0200

    Improve DependencyChecker using Dictionary

--- TestAccountFixes/Dependencies/DependencyChecker.cs
+++ TestAccountFixes/Dependencies/DependencyChecker.cs
@@ -1,21 +1,25 @@
+using System.Collections.Generic;
 using System.Linq;
 using BepInEx.Bootstrap;
+using UnityEngine.UIElements.Collections;
 
 namespace TestAccountFixes.Dependencies;
 
 internal static class DependencyChecker {
-    internal static bool IsLobbyCompatibilityInstalled() =>
-        TestAccountCore.Dependencies.DependencyChecker.IsLobbyCompatibilityInstalled();
+    private static readonly Dictionary<string, bool> _DependencyDictionary = [
+    ];
 
-    internal static bool IsBetterItemHandlingInstalled() =>
-        Chainloader.PluginInfos.Values.Any(metadata =>
-                                               metadata.Metadata.GUID.Contains("Yan01h.BetterItemHandling"));
+    internal static bool IsLobbyCompatibilityInstalled() => TestAccountCore.Dependencies.DependencyChecker.IsLobbyCompatibilityInstalled();
 
-    internal static bool IsTelevisionControllerInstalled() =>
-        Chainloader.PluginInfos.Values.Any(metadata =>
-                                               metadata.Metadata.GUID.Contains("KoderTech.TelevisionController"));
+    internal static bool IsBetterItemHandlingInstalled() => IsInstalled("Yan01h.BetterItemHandling");
 
-    internal static bool IsInventoryFixPluginInstalled() =>
-        Chainloader.PluginInfos.Values.Any(metadata =>
-                                               metadata.Metadata.GUID.Contains("Dokge.InventoryFixPlugin"));
+    internal static bool IsTelevisionControllerInstalled() => IsInstalled("KoderTech.TelevisionController");
+
+    internal static bool IsInventoryFixPluginInstalled() => IsInstalled("Dokge.InventoryFixPlugin");
+
+    private static bool IsInstalled(string key) {
+        return _DependencyDictionary.ContainsKey(key)
+            ? _DependencyDictionary.Get(key)
+            : Chainloader.PluginInfos.Values.Any(metadata => metadata.Metadata.GUID.Contains(key));
+    }
 }
